015a0e0bd50ab99dcd5c6b1dd1f61d02f2b75594
= <aalouane.s@gmail.com>
2024-01-04 09:25:08 +0100

    Controllers 3 AuthController

---
+++ controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\core\Request;
+
+class AuthController extends Controller
+{
+
+  public function login(Request $request)
+  {
+    
+    return $this->render("login");
+  }
+
+  public function register(Request $request)
+  {
+    return $this->render("register");
+  }
+}
